ef03f3e05c266894ec5f3eb983b34a40786155e0
Alisher <alisher@MacBook-Air-Alisher.local>
2025-04-05 23:29:05 +0300

    fix: исправлен импорт TelegramProvider в App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { HashRouter } from 'react-router-dom'
-import { TelegramProvider } from './providers/TelegramProvider'
+import { TelegramProvider } from './providers/TelegramProvider/TelegramProvider'
 import { QueryProvider } from './providers/QueryProvider'
 import { SocketProvider } from './providers/SocketProvider'
 import { Router } from './Router'
